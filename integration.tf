ba93d044f78896624beba449c12be744cc811814
ambikabengaluru <ambikagithub@gmail.com>
2026-07-09 14:52:09 +0530

    Create integration.tf

---
+++ integration.tf
@@ -0,0 +1,13 @@
+resource "snowflake_storage_integration" "s3_integration" {
+  name    = var.integration_name
+  type    = "EXTERNAL_STAGE"
+
+  storage_provider    = "S3"
+  storage_aws_role_arn = var.aws_role_arn
+
+  enabled = true
+
+  storage_allowed_locations = [
+    var.s3_location
+  ]
+}
