ba2c377a95d2b79d9d4e8673eea83212a402a17f
ambikabengaluru <ambikagithub@gmail.com>
2026-07-10 17:15:28 +0530

    Create pipe.tf

---
+++ pipe.tf
@@ -0,0 +1,19 @@
+resource "snowflake_pipe" "sales_pipe" {
+
+  database = snowflake_database.sample_db.name
+  schema   = snowflake_schema.sample_schema.name
+
+  name = var.pipe_name
+
+  copy_statement = <<EOT
+COPY INTO sample_db.sample_SCHEMA.EXAMPLE
+FROM @SAMPLE_DB.SAMPLE_SCHEMA.EXT_STG
+FILE_FORMAT = (
+    TYPE = CSV
+    FIELD_DELIMITER = ','
+    SKIP_HEADER = 1
+)
+EOT
+
+  auto_ingest = false
+}
