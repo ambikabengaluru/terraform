802d6240351052739d6a7abd6e13fe61a5858d67
ambikabengaluru <ambikagithub@gmail.com>
2026-07-10 15:35:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-provider "snowflake" {
-  organization_name = var.organization_name
-  account_name      = var.account_name
-  user              = var.user
-  password          = var.password
-
-  role              = var.role
-  warehouse         = var.warehouse
-}
 
 resource "snowflake_database" "sales_db" {
   name = var.database
