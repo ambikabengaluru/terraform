fe86cbbb62cecb016f8b5335e8ed23e5caa3b1e5
ambikabengaluru <ambikagithub@gmail.com>
2026-07-10 16:57:25 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -21,6 +21,7 @@ provider "snowflake" {
   warehouse = var.warehouse
 
   preview_features_enabled = [
-    "snowflake_storage_integration_resource"
+    "snowflake_storage_integration_resource",
+    "snowflake_stage_resource"
   ]
 }
