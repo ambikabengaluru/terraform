d327df23473155be6534c434b5cc21cc92e2cb84
ambikabengaluru <ambikagithub@gmail.com>
2026-07-10 15:31:20 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,3 +8,19 @@ terraform {
 
   required_version = ">= 1.5.0"
 }
+
+
+provider "snowflake" {
+  organization_name = var.organization_name
+  account_name      = var.account_name
+
+  user      = var.user
+  password  = var.password
+
+  role      = var.role
+  warehouse = var.warehouse
+
+  preview_features_enabled = [
+    "snowflake_storage_integration_resource"
+  ]
+}
