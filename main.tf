2445c3e28cfcc1fcac3af5c8c0f6ad97c374d637
ambikabengaluru <ambikagithub@gmail.com>
2026-07-10 15:46:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 
-resource "snowflake_database" "sale_db" {
+resource "snowflake_database" "test_db" {
   name = var.database
 }
 
-resource "snowflake_schema" "sale_schema" {
-  database = snowflake_database.sale_db.name
+resource "snowflake_schema" "test_schema" {
+  database = snowflake_database.test_db.name
   name      = var.schema
 }
