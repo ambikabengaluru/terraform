49c7961eb7df09fd9051b09c36c2f1f80aa29696
ambikabengaluru <ambikagithub@gmail.com>
2026-07-20 10:38:25 +0530

    Update stage.tf

--- stage.tf
+++ stage.tf
@@ -1,10 +1,10 @@
 resource "snowflake_stage" "external_stage" {
 
   name     = var.stage_name
-  database = snowflake_database.sample_db.name
-  schema   = snowflake_schema.sample_schema.name
+  database = snowflake_database.unique_db.name
+  schema   = snowflake_schema.unique_schema.name
 
   url = var.s3_location
 
-  storage_integration = snowflake_storage_integration.s3_my.name
+  storage_integration = snowflake_storage_integration.s3_my_bucket.name
 }
